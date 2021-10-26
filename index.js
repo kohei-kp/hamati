36c7f064856716e64a3f23501c698f0e34fef6ea
kohei-kp <cowhey1991@gmail.com>
2021-10-26 12:06:25 +0900

    feat: smislot sample

--- index.js
+++ index.js
@@ -6,8 +6,47 @@ const router = Router()
 /*
 Our index route, a simple hello world.
 */
-router.get("/", () => {
-  return new Response("Hello, world! This is the root page of your Worker template.")
+router.get('/', () => {
+    return new Response(
+        'Hello, world! This is the root page of your Worker template.'
+    )
+})
+
+/**
+ * umislot
+ */
+router.get('/umislot', () => {
+    const rand = () => {
+        return Math.floor(Math.random() * 4)
+    }
+
+    const veries = [':very:', ':very-left', ':very-right:', ':very-bottom:']
+    const longs = [':long:', ':long-left', ':long-right:', ':long-bottom:']
+    const umityans = [
+        ':umityan:',
+        ':umityan-left',
+        ':umityan-right:',
+        ':umityan-bottom:',
+    ]
+
+    const textLines = [veries[rand()], longs[rand()], umityans[rand()]]
+    const blocks = [
+        {
+            type: 'section',
+            text: {
+                type: 'text',
+                text: textLines.join('\n'),
+            },
+        },
+    ]
+
+    return new Response(
+        JSON.stringify({
+            blocks,
+            resonse_type: 'in_channel',
+        }),
+        { headers: { 'Content-type': 'application/json' } }
+    )
 })
 
 /*
@@ -16,22 +55,22 @@ URL.
 
 Try visit /example/hello and see the response.
 */
-router.get("/example/:text", ({ params }) => {
-  // Decode text like "Hello%20world" into "Hello world"
-  let input = decodeURIComponent(params.text)
+router.get('/example/:text', ({ params }) => {
+    // Decode text like "Hello%20world" into "Hello world"
+    let input = decodeURIComponent(params.text)
 
-  // Construct a buffer from our input
-  let buffer = Buffer.from(input, "utf8")
+    // Construct a buffer from our input
+    let buffer = Buffer.from(input, 'utf8')
 
-  // Serialise the buffer into a base64 string
-  let base64 = buffer.toString("base64")
+    // Serialise the buffer into a base64 string
+    let base64 = buffer.toString('base64')
 
-  // Return the HTML with the string to the client
-  return new Response(`<p>Base64 encoding: <code>${base64}</code></p>`, {
-    headers: {
-      "Content-Type": "text/html"
-    }
-  })
+    // Return the HTML with the string to the client
+    return new Response(`<p>Base64 encoding: <code>${base64}</code></p>`, {
+        headers: {
+            'Content-Type': 'text/html',
+        },
+    })
 })
 
 /*
@@ -43,26 +82,26 @@ Try the below curl command to send JSON:
 
 $ curl -X POST <worker> -H "Content-Type: application/json" -d '{"abc": "def"}'
 */
-router.post("/post", async request => {
-  // Create a base object with some fields.
-  let fields = {
-    "asn": request.cf.asn,
-    "colo": request.cf.colo
-  }
-
-  // If the POST data is JSON then attach it to our response.
-  if (request.headers.get("Content-Type") === "application/json") {
-    fields["json"] = await request.json()
-  }
-
-  // Serialise the JSON to a string.
-  const returnData = JSON.stringify(fields, null, 2);
-
-  return new Response(returnData, {
-    headers: {
-      "Content-Type": "application/json"
+router.post('/post', async request => {
+    // Create a base object with some fields.
+    let fields = {
+        asn: request.cf.asn,
+        colo: request.cf.colo,
     }
-  })
+
+    // If the POST data is JSON then attach it to our response.
+    if (request.headers.get('Content-Type') === 'application/json') {
+        fields['json'] = await request.json()
+    }
+
+    // Serialise the JSON to a string.
+    const returnData = JSON.stringify(fields, null, 2)
+
+    return new Response(returnData, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
 })
 
 /*
@@ -71,12 +110,12 @@ above, therefore it's useful as a 404 (and avoids us hitting worker exceptions, 
 
 Visit any page that doesn't exist (e.g. /foobar) to see it in action.
 */
-router.all("*", () => new Response("404, not found!", { status: 404 }))
+router.all('*', () => new Response('404, not found!', { status: 404 }))
 
 /*
 This snippet ties our worker to the router we deifned above, all incoming requests
 are passed to the router where your routes are called and the response is sent.
 */
-addEventListener('fetch', (e) => {
-  e.respondWith(router.handle(e.request))
+addEventListener('fetch', e => {
+    e.respondWith(router.handle(e.request))
 })
